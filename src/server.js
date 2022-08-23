6afe85cdf58b58c001e3f20dc3e6d2e2d553c58f
Jabbor <sattarovjabboro4@gmail.com>
2022-08-23 13:26:01 +0500

    second change

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
 const express = require("express")
 const fs = require('fs')
 const path = require('path')
+const jwt = require("jsonwebtoken")
+const sha256 = require('sha256')
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+
+
+app.post("/sign-up", (req, res)=>{
+
+})
+
 
 app.get('/users', (req, res)=> {
     let users =JSON.parse(fs.readFileSync(path.join(process.cwd(), 'src', "database", "users.json"), "utf-8")) || []
